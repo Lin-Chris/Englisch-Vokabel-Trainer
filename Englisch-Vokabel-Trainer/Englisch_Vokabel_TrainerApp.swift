c921d0bfad34b73972465393f325c1ffd8e88b79
Christian <ch_linder@posteo.co.uk>
2026-02-01 16:20:33 +0100

    Initial Commit

---
+++ Englisch-Vokabel-Trainer/Englisch_Vokabel_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Englisch_Vokabel_TrainerApp.swift
+//  Englisch-Vokabel-Trainer
+//
+//  Created by Christian on 01.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Englisch_Vokabel_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Englisch-Vokabel-TrainerTests/Englisch_Vokabel_TrainerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Englisch_Vokabel_TrainerTests.swift
+//  Englisch-Vokabel-TrainerTests
+//
+//  Created by Christian on 01.02.26.
+//
+
+import Testing
+@testable import Englisch_Vokabel_Trainer
+
+struct Englisch_Vokabel_TrainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
